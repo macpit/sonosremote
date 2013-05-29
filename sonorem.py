b3dbaddcac4a6c26f8752ca2d5d7540853ec8362
Marc Peter <marc.peter@swissonline.ch>
2013-05-29 15:43:25 +0200

    Add, auto ip and iplist

--- sonorem.py
+++ sonorem.py
@@ -1,115 +1,152 @@
 #!/usr/bin/python
 
-# lrvic - https://github.com/lrvick/raspi-hd44780/blob/master/hd44780.py
-# LiquidCrystal - https://github.com/arduino/Arduino/blob/master/libraries/LiquidCrystal/LiquidCrystal.cpp
-
 import logging
-logging.basicConfig(level=logging.INFO)
+
+logging.basicConfig()
 
 import thread
-import random
+
 from time import sleep
 
-from Adafruit_I2C import Adafruit_I2C
 from Adafruit_CharLCDPlate import Adafruit_CharLCDPlate
 
 lcd = Adafruit_CharLCDPlate()
 lcd.begin(16, 2)
 lcd.clear()
 
-col = (('Red' , lcd.RED) , ('Yellow', lcd.YELLOW), ('Green' , lcd.GREEN),
-       ('Teal', lcd.TEAL), ('Blue'  , lcd.BLUE)  , ('Violet', lcd.VIOLET))
+col = (('Red', lcd.RED), ('Yellow', lcd.YELLOW), ('Green', lcd.GREEN),
+       ('Teal', lcd.TEAL), ('Blue', lcd.BLUE), ('Violet', lcd.VIOLET))
 
 from core import SoCo
 from core import SonosDiscovery
 
-sonosip = SonosDiscovery()
-iplist=sonosip.get_speaker_ips()
-#ip=iplist[random.randint(0, len(iplist))]
-ip=iplist[0]
+
+def get_master_ip():
+    _sonosip = SonosDiscovery()
+    _getsp = _sonosip.get_speaker_ips()
+
+    for i in _getsp:
+        try:
+            _tst = SoCo(i)
+            #print _tst.get_speaker_info()['zone_name']
+            if _tst.get_current_transport_info()['current_transport_state'] == 'PLAYING':
+                #print "Playing --> ",i
+                if _tst.get_current_track_info()['duration'] != 'NOT_IMPLEMENTED':
+                    #print _tst.get_current_track_info()
+                    _ip = i
+        except:
+            pass
+    return _ip
 
 
-def keys(ip, iplist):
-	sonos = SoCo(ip)
-	vol = sonos.volume()
-	bas = sonos.bass()
+def get_master_iplist():
+    _iplist = []
+    _sonosip = SonosDiscovery()
+    _getsp = _sonosip.get_speaker_ips()
 
-	print ip
-	print iplist
+    for i in _getsp:
+        try:
+            _tst = SoCo(i)
+            _tst.get_speaker_info()['zone_name']
+            _iplist.append(i)
+        except:
+            pass
+    return _iplist
 
-	# Init display
-	msg = 'Vol %s' % vol
-	lcd.setCursor(0,0)
-	lcd.message(msg)
-	msg = 'Bass %s' % bas
-	lcd.setCursor(8,0)
-	lcd.message(msg)
 
-	while True:
-		if lcd.buttonPressed(lcd.UP):
-			for i in iplist:
-				sonos = SoCo(ip)
-				sonos.volume(vol)
-			vol += 1
-			msg = 'Vol %s' % vol
-			lcd.setCursor(0,0)
-			lcd.message(msg)
+def set_speaker_default():
+    for i in get_master_iplist():
+        try:
+            print "Setup -->", i
+            sonos = SoCo(i)
+            sonos.mute(False)
+            sonos.volume(35)
+            sonos.bass(5)
+            sonos.set_loudness(True)
+        except:
+            pass
 
-		if lcd.buttonPressed(lcd.DOWN):
-			for i in iplist:
-				sonos = SoCo(ip)
-				sonos.volume(vol)
-			vol -= 1
-			msg = 'Vol %s' % vol
-			lcd.setCursor(0,0)
-			lcd.message(msg)
 
-		if lcd.buttonPressed(lcd.LEFT):
-			for i in iplist:
-				sonos = SoCo(ip)
-				sonos.bass(bas)
-			bas -= 1
-			msg = 'Bass %s' % bas
-			lcd.setCursor(8,0)
-			lcd.message(msg)
+def keys():
+    sonos = SoCo(get_master_ip())
+    vol = sonos.volume()
+    bas = sonos.bass()
 
-		if lcd.buttonPressed(lcd.RIGHT):
-			for i in iplist:
-				sonos = SoCo(ip)
-				sonos.bass(bas)
-			bas += 1
-			msg = 'Bass %s' % bas
-			lcd.setCursor(8,0)
-			lcd.message(msg)
+    # Init display
+    msg = 'Vol %s' % vol
+    lcd.setCursor(0, 0)
+    lcd.message(msg)
+    msg = 'Bass %s' % bas
+    lcd.setCursor(8, 0)
+    lcd.message(msg)
 
-	sleep(0.1)
+    _iplist = get_master_iplist()
 
-def display (ip):
-	sonos = SoCo(ip)
-	lcd.setCursor(0,1)
+    while True:
+        if lcd.buttonPressed(lcd.UP):
+            vol += 1
+            for i in _iplist:
+                sonos = SoCo(i)
+                sonos.volume(vol)
+            msg = 'Vol %s' % vol
+            lcd.setCursor(0, 0)
+            lcd.message(msg)
 
-	while True:
-		try:
-			tr=sonos.get_current_track_info()
-			msg = msg=tr['artist']+" , "+tr['title']+" "
-			mlen=len(msg)
-		except:
-			pass
+        if lcd.buttonPressed(lcd.DOWN):
+            vol -= 1
+            for i in _iplist:
+                sonos = SoCo(i)
+                sonos.volume(vol)
+            msg = 'Vol %s' % vol
+            lcd.setCursor(0, 0)
+            lcd.message(msg)
 
-		for i in range (0, len(msg)):
-			lcd.setCursor(0,1)
-			str = msg[i:(i+16)]
-			lcd.message(str)
-			# Wait for text is start scrolling
-			if i == 0:
-				sleep (2)
-			sleep (0.4)
+        if lcd.buttonPressed(lcd.LEFT):
+            bas -= 1
+            for i in _iplist:
+                sonos = SoCo(i)
+                sonos.bass(bas)
+            msg = 'Bass %s' % bas
+            lcd.setCursor(8, 0)
+            lcd.message(msg)
 
-thread.start_new_thread(keys,(ip, iplist,))
-thread.start_new_thread(display,(ip,))
+        if lcd.buttonPressed(lcd.RIGHT):
+            bas += 1
+            for i in _iplist:
+                sonos = SoCo(i)
+                sonos.bass(bas)
+            msg = 'Bass %s' % bas
+            lcd.setCursor(8, 0)
+            lcd.message(msg)
+
+    sleep(0.1)
+
+
+def display():
+    _sonos = SoCo(get_master_ip())
+    lcd.setCursor(0, 1)
+
+    while True:
+        try:
+            tr = _sonos.get_current_track_info()
+            msg = msg = tr['artist'] + " , " + tr['title'] + " "
+            mlen = len(msg)
+        except:
+            pass
+
+        for i in range(0, len(msg)):
+            lcd.setCursor(0, 1)
+            str = msg[i:(i + 16)]
+            lcd.message(str)
+            # Wait for text is start scrolling
+            if i == 0:
+                sleep(2)
+            sleep(0.4)
+
+set_speaker_default()
+thread.start_new_thread(keys, (), )
+thread.start_new_thread(display, (), )
 
 while True:
-	pass
-	# title=sonos.get_current_track_info()
-	# msg="Song: "+title['title']+" Artist: "+title['artist']
+    pass
 
